7e4b26c955992112eb4220e0b5e7c6b1e39dc6b6
DavidVamaiotu <159152663+DavidVamaiotu@users.noreply.github.com>
2026-09-23 21:32:22 +0300

    Add MockWebServer for stable AutoSync tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -574,6 +574,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
     testImplementation("io.mockk:mockk:1.13.12")
+    testImplementation("com.squareup.okhttp3:mockwebserver:5.3.2")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
